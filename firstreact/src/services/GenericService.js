cfe6dba520584531f12990b925bd6358762d36c5
somantalha <somantalha98@gmail.com>
2021-06-26 01:03:32 +0500

    starting heroku deployment

--- firstreact/src/services/GenericService.js
+++ firstreact/src/services/GenericService.js
@@ -56,8 +56,8 @@
 
 import axios from "axios";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
-axios.defaults.baseURL = "http://localhost:4000/api/";
-// axios.defaults.baseURL = "/api/";
+// axios.defaults.baseURL = "http://localhost:4000/api/";
+axios.defaults.baseURL = "/api/";
 class GenericService {
   constructor() {}
   get = (url) =>
